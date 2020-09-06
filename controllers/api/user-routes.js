360ac209920ca5f8008684cd213d7705ce7274a7
Kennadee <kennadee.ray@gmail.com>
2020-09-05 21:23:59 -0500

    Postmodel created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,6 +64,13 @@ router.post('/login', (req, res) => {
         res.json({ user: dbUserData });
     
         // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
     
       });  
     });
