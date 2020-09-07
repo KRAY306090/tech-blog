e195aa51465f82776f53e65bf48e1d481242134e
Kennadee <kennadee.ray@gmail.com>
2020-09-07 17:06:28 -0500

    update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,14 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
   
 
 module.exports = router;
