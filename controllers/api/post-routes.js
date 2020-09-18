5b63dc772a0f3043d8aac724e628004ea30b6573
Kennadee <kennadee.ray@gmail.com>
2020-09-18 12:05:44 -0500

    dashboard functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -70,7 +70,7 @@ router.post('/', (req, res) => {
   Post.create({
     title: req.body.title,
     post_text: req.body.post_text,
-    user_id: req.body.user_id
+    user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
