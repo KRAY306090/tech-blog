f7e8d463909dd60174a603d32cc351bfa8b21074
Kennadee <kennadee.ray@gmail.com>
2020-09-17 21:31:14 -0500

    single post view and comments added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,16 +11,19 @@ router.get('/', (req, res) => {
   });
 
 router.post('/', (req, res) => {
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 router.delete('/:id', (req, res) => {
